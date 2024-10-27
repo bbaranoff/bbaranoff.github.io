2df947cd2b48ba5932fdb3fbaebecf13c45c6f38
bbaranoff <bastienbaranoff@hotmail.fr>
2024-10-27 14:58:40 +0100

    commit

--- _includes/ga.js
+++ _includes/ga.js
@@ -1,7 +1,18 @@
-<script src="https://www.googletagmanager.com/gtag/js?id=464931235" type="text/plain" class="ccb-cookie-consent" ccb-cookie-type="ccb_analytics"></script>
-<script type="text/plain" class="ccb-cookie-consent" ccb-cookie-type="ccb_analytics">
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', '464931235', { 'anonymize_ip': true });
-</script>
+function loadScriptAsync(scriptSrc, callback) {
+    if (typeof callback !== 'function') {
+        throw new Error('Not a valid callback for async script load');
+    }
+    var script = document.createElement('script');
+    script.onload = callback;
+    script.src = scriptSrc;
+    document.head.appendChild(script);
+}
+
+/* This is the part where you call the above defined function and "calls back" your code which gets executed after the script has loaded */
+
+loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=G-464931235', function () {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-464931235', { 'anonymize_ip': true });
+})
