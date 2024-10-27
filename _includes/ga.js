35cee51a823884abda6e6c98ee59b25903532a02
bbaranoff <bastienbaranoff@hotmail.fr>
2024-10-27 14:11:40 +0100

    commit

--- _includes/ga.js
+++ _includes/ga.js
@@ -1,11 +1,7 @@
-<!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=464931235"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'TAG_ID
-
-');
-  </script>
+<script src="https://www.googletagmanager.com/gtag/js?id=464931235" type="text/plain" class="ccb-cookie-consent" ccb-cookie-type="ccb_analytics"></script>
+<script type="text/plain" class="ccb-cookie-consent" ccb-cookie-type="ccb_analytics">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '464931235', { 'anonymize_ip': true });
+</script>
