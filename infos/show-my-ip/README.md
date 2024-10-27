6f9411105084cf12a48810acba21e78e2cedab9e
bbaranoff <bastienbaranoff@hotmail.fr>
2024-10-27 23:13:21 +0100

    cmo

---
+++ infos/show-my-ip/README.md
@@ -0,0 +1,5 @@
+# show my ip
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/vardump/pen/YxVNbb](https://codepen.io/vardump/pen/YxVNbb).
+
+small Typescript async / await test. 
--- infos/show-my-ip/src/script.ts
+++ infos/show-my-ip/src/script.ts
@@ -5,6 +5,6 @@ async function getIP(): Promise<string> {
     const element_ip = document.getElementById("vd_ip") || new HTMLElement();
     const promise = await fetch("//ipinfo.io/json");
     const data = await promise.json();
-    element_ip.innerHTML = data.ip + " | " + data.hostname +  " | " + data.city>
+    element_ip.innerHTML = data.ip + " | " + data.hostname +  " | " + data.city + " | " + data.country ;
     return data;
 }
