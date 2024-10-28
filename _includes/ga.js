18c67ba2c4a49a011f244b0b4f67168314833566
bbaranoff <bastienbaranoff@hotmail.fr>
2024-10-28 17:08:51 +0100

    commit

--- _includes/ga.js
+++ _includes/ga.js
@@ -1,9 +1,9 @@
 <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-464931235"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VYSL22WMS1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    gtag('config', 'G-464931235');
+  gtag('config', 'G-VYSL22WMS1');
 </script>
