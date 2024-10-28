248a36b8c6cb2b44ec7fd149a50fa8b992933c88
bbaranoff <bastienbaranoff@hotmail.fr>
2024-10-28 22:20:10 +0100

    commit

--- _includes/ga.js
+++ _includes/ga.js
@@ -1,18 +1,9 @@
-function loadScriptAsync(scriptSrc, callback) {
-    if (typeof callback !== 'function') {
-        throw new Error('Not a valid callback for async script load');
-    }
-    var script = document.createElement('script');
-    script.onload = callback;
-    script.src = scriptSrc;
-    document.head.appendChild(script);
-}
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VYSL22WMS1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-/* This is the part where you call the above defined function and "calls back" your code which gets executed after the script has loaded */
-
-loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=G-VYSL22WMS1', function () {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-    gtag('config', ' G-VYSL22WMS1', { 'anonymize_ip': true });
-})
+  gtag('config', 'G-VYSL22WMS1');
+</script>
