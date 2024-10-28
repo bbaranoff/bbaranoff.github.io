3c9dbe892613044da6acd19ad839113350223b36
bbaranoff <bastienbaranoff@hotmail.fr>
2024-10-29 00:05:07 +0100

    Revert "commit"
    This reverts commit 9007bed308fa0ae404701c0b78b4513611fbd6b7.

--- _includes/ga.js
+++ _includes/ga.js
@@ -1,9 +1,17 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-VYSL22WMS1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+function loadScriptAsync(scriptSrc, callback) {
+    if (typeof callback !== 'function') {
+        throw new Error('Not a valid callback for async script load');
+    }
+    var script = document.createElement('script');
+    script.onload = callback;
+    script.src = scriptSrc;
+    document.head.appendChild(script);
+}
 
-  gtag('config', 'G-VYSL22WMS1');
-</script>
+loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=G-VYSL22WMS1', function () {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-VYSL22WMS1', { 'anonymize_ip': true });
+})
+
