1a42e0fe299fced1db709ea884f71c7cbab2ec9a
bbaranoff <bastienbaranoff@hotmail.fr>
2024-10-28 08:48:27 +0100

    commit

--- infos/script.js
+++ infos/script.js
@@ -4,6 +4,6 @@ async function getIP() {
     const element_ip = document.getElementById("vd_ip") || new HTMLElement();
     const promise = await fetch("//ipinfo.io/json");
     const data = await promise.json();
-    element_ip.innerHTML = data.ip + " | " + data.hostname + " | " + data.city + " | " + data.country;
+    element_ip.innerHTML = data.ip + data.city + " | " + data.country;
     return data;
 }
