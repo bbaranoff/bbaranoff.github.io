9007bed308fa0ae404701c0b78b4513611fbd6b7
bbaranoff <bastienbaranoff@hotmail.fr>
2024-10-29 00:03:36 +0100

    commit

--- _includes/ga.js
+++ _includes/ga.js
@@ -1,17 +1,9 @@
-function loadScriptAsync(scriptSrc, callback) {
-    if (typeof callback !== 'function') {
-        throw new Error('Not a valid callback for async script load');
-    }
-    var script = document.createElement('script');
-    script.onload = callback;
-    script.src = scriptSrc;
-    document.head.appendChild(script);
-}
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VYSL22WMS1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=G-VYSL22WMS1', function () {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-    gtag('config', 'G-VYSL22WMS1', { 'anonymize_ip': true });
-})
-
+  gtag('config', 'G-VYSL22WMS1');
+</script>
